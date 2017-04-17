ee380b4b653f10f762adf6026209880d67659a7a
SuperVe_hacker <huavinhvietanh@gmail.com>
2017-04-17 20:51:06 +0700

    kjhgjk

---
+++ convert F to C.py
@@ -0,0 +1,5 @@
+
+while True:
+    C = float(input("Enter the temperature in Celsius! "))
+    F = 1.8 * C + 32 
+    print (F)
---
+++ multi circle.py
@@ -0,0 +1,9 @@
+from turtle import*
+shape("arrow")
+speed (-1)
+color ( "green", "white")
+begin_fill ()
+for i  in range (150):
+        circle (69)
+        left (360/150)
+end_fill ()
